ec92803290a130b7bc6960ac0a2cded4b10234a7
redeyeshu007 <sudharsanelangovan2005@gmail.com>
2026-05-09 19:14:54 +0530

    Fix: Add SMTP timeout to prevent hanging on slow connections

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -20,6 +20,9 @@ const sendEmail = async ({ to, subject, html }) => {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
+      connectionTimeout: 10000, // 10 seconds
+      greetingTimeout: 10000,
+      socketTimeout: 10000,
     });
     try {
       await transporter.sendMail({
